9eb171c816a5ea91e4344a733a2a6e135226b241
Shilo <shilo86@gmail.com>
2026-04-05 09:10:27 -0700

    feat: add syllable-based name generator with tests

---
+++ Core.Tests/PolyPetNameGeneratorTests.cs
@@ -0,0 +1,52 @@
+using Xunit;
+using PolyPet;
+
+namespace PolyPet.Tests
+{
+    public class PolyPetNameGeneratorTests
+    {
+        [Fact]
+        public void Create_ReturnsSameNameForSameSeed()
+        {
+            var name1 = PolyPetNameGenerator.Create(42);
+            var name2 = PolyPetNameGenerator.Create(42);
+            Assert.Equal(name1, name2);
+        }
+
+        [Fact]
+        public void Create_ReturnsDifferentNamesForDifferentSeeds()
+        {
+            var name1 = PolyPetNameGenerator.Create(1);
+            var name2 = PolyPetNameGenerator.Create(999);
+            Assert.NotEqual(name1, name2);
+        }
+
+        [Fact]
+        public void Create_ReturnsCapitalizedName()
+        {
+            var name = PolyPetNameGenerator.Create(42);
+            Assert.True(char.IsUpper(name[0]));
+        }
+
+        [Fact]
+        public void Create_ReturnsNameWith4To9Characters()
+        {
+            // 2 syllables = 4-6 chars, 3 syllables = 6-9 chars
+            for (int seed = 0; seed < 100; seed++)
+            {
+                var name = PolyPetNameGenerator.Create(seed);
+                Assert.InRange(name.Length, 4, 9);
+            }
+        }
+
+        [Fact]
+        public void Create_ReturnsOnlyLetters()
+        {
+            for (int seed = 0; seed < 100; seed++)
+            {
+                var name = PolyPetNameGenerator.Create(seed);
+                Assert.All(name.ToCharArray(), c => Assert.True(char.IsLetter(c)));
+            }
+        }
+    }
+}
